f1fc606963ee204799cac21189f74e2c3138774e
vitorcontieri <vitor.pecanha@hotmail.com>
2021-07-15 17:56:33 -0300

    abri e fechei os arqv

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Aluno: Vitor Contieri Rezende Peçanha
+Número USP: 10387706
+"""
+
+#abertura do arquivo de entrada
+entrada = open('entrada.txt', 'r')
+
+#abertura do arquivo de saida
+saida = open('saida.txt', 'w')
+
+
+#fechando os arquivos
+entrada.close()
+saida.close()
