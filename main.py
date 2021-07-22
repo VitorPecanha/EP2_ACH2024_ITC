b2153439bdee0204fe8ecb9fe82013f6318c89de
vitorcontieri <vitor.pecanha@hotmail.com>
2021-07-22 17:15:20 -0300

    Corrigi uma condição de existência, e criei a variável de conteúdo

--- main.py
+++ main.py
@@ -7,11 +7,22 @@ Número USP: 10387706
 entrada = open('entrada.txt', 'r')
 
 #abertura do arquivo de saida
+saida = open('saida.txt', 'r')
+
+#variável de conteudo
+conteudo = saida.readlines()
+saida.close()
+
 saida = open('saida.txt', 'w')
+
 #lendo todas as linhas do arquivo
 #e guardando em 'n' o número de gramáticas (primeiro elemento do arquivo)
 linhas = entrada.readlines()
-n = linhas[0] if int(linhas[0]) >= 0 else saida.write('ERRO 1\n')
+n = linhas[0]
+
+if int(n) < 0:
+    conteudo.append('ERRO 1\n')
+    quit()
 
 posicao = 1
 
@@ -21,7 +32,7 @@ for i in range(0, int(n)):
     r1 = int(linhas[posicao][4])
 
     if v1 < 0 or t1 < 0 or r1 < 0:
-        saida.write('ERRO 1\n')
+        conteudo.append('ERRO 1\n')
         pass
 
     aux = posicao + 1
@@ -29,10 +40,12 @@ for i in range(0, int(n)):
 
     regras = []
     while aux < posicao:
-        regras.write(linhas[aux])
+        regras.append(linhas[aux])
         aux += 1
 
 
+
+saida.writelines(conteudo)
 
 #fechando os arquivos
 entrada.close()
