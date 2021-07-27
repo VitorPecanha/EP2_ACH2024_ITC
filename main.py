46f1f58728ca63ba03cdfbb6219083045d8d5b2c
vitorcontieri <vitor.pecanha@hotmail.com>
2021-07-27 16:10:37 -0300

    resolvido problema de separar caracteres

--- main.py
+++ main.py
@@ -7,35 +7,28 @@ Número USP: 10387706
 entrada = open('entrada.txt', 'r')
 
 #abertura do arquivo de saida
-saida = open('saida.txt', 'r')
-
-#variável de conteudo
-conteudo = saida.readlines()
-saida.close()
-
-saida = open('saida.txt', 'w')
+saida = open('saida.txt', 'a')
 
 #lendo todas as linhas do arquivo
 #e guardando em 'n' o número de gramáticas (primeiro elemento do arquivo)
 linhas = entrada.readlines()
-n = linhas[0]
+n = int(linhas[0])
 
-if int(n) < 0:
-    conteudo.append('ERRO 1\n')
+if n < 0:
+    saida.write('ERRO 1\n')
     quit()
 
 posicao = 1
 
 for i in range(0, int(n)):
-    v1 = int(linhas[posicao][0])
-    t1 = int(linhas[posicao][2])
-    r1 = int(linhas[posicao][4])
+    var = linhas[posicao].split()
+
+    v1 = int(var[0])
+    t1 = int(var[1])
+    r1 = int(var[2])
 
     if v1 < 0 or t1 < 0 or r1 < 0:
-        conteudo.append('ERRO 1\n')
-        continue
-
-    print('oi')
+        saida.write('ERRO 1\n')
 
     aux = posicao + 1
     posicao = posicao + 3 + r1
@@ -44,10 +37,6 @@ for i in range(0, int(n)):
     while aux < posicao:
         regras.append(linhas[aux])
         aux += 1
-
-
-
-saida.writelines(conteudo)
 
 #fechando os arquivos
 entrada.close()
